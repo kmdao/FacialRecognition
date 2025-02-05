4d1ed654b71e68ce959d8b53ad6bd3772e0e4614
kmdao <89680148+kmdao@users.noreply.github.com>
2025-02-05 11:18:19 -0800

    initial image

--- facialrecognition.py
+++ facialrecognition.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 data = np.genfromtxt('training_images.csv', delimiter=',')
 print(data.shape)
 
-# Reshape the data so we have square images
+# Reshape each row vector of the original data set, so we have a 300x300 array as input for imshow()
 h = 300
 w = 300
 
@@ -13,3 +13,4 @@ img = np.reshape(data[0, :], [h, w], order='F')
 # show image
 plt.imshow(img, cmap='gray')
 plt.show()
+
