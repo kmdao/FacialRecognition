6cd5be8ae46b3167304cccc2359370f70bb868c6
kmdao <89680148+kmdao@users.noreply.github.com>
2025-02-04 20:16:49 -0800

    initial image

--- facialrecognition.py
+++ facialrecognition.py
@@ -1,5 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-x = np.sin(1)
-print(x)
 
+data = np.genfromtxt('training_images.csv', delimiter=',')
+print(data.shape)
+
+# Reshape the data so we have square images
+h = 300
+w = 300
+
+img = np.reshape(data[0, :], [h, w], order='F')
+
+# show image
+plt.imshow(img, cmap='gray')
+plt.show()
